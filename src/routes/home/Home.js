f46154e0f93232abb3814d9379cdce991b44a325
Terri <terri@OT-MacBook-Pro.local>
2017-01-24 17:31:13 -0800

    Change message color

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -53,10 +53,10 @@ class Home extends React.Component {
 
     try{
         parsed = this.processJson(this.state.value);
-        this.setState({error: "Finished parsing."});
+        this.setState({error: {type:'success', message:"Finished parsing."}});
         this.setState({value: parsed});
     } catch (e){
-        this.setState({error: e.name + " : " + e.message});
+        this.setState({error: {type:'error', message:e.name + " : " + e.message}});
     }
 
   }
@@ -67,7 +67,7 @@ class Home extends React.Component {
           <h2 className={s.title}>Place your json object here:</h2>
              <div>{(function(error) {
                       if (error) {
-                        return (<div>{error}</div>);
+                        return (<div className={error.type === 'error'?s.error:s.success}>{error.message}</div>);
                       }
                     })(this.state.error)}</div>
           <textarea rows="20" placeholder="Put your JSON object here." cols="120" value={this.state.value} onChange={this.handleChange}>
